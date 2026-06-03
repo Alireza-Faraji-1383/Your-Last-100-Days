e508cd4d1e73ba01b4be332b6a51f00a2de2e5cb
Alireza-Faraji-1383 <farajialireza664@gmail.com>
2026-06-03 22:46:08 +0400

    balance enigmaticlegacyplus

--- kubejs/server_scripts/loot/artifacts.js
+++ kubejs/server_scripts/loot/artifacts.js
@@ -12,4 +12,22 @@ LootJS.modifiers((event) => {
       if (Math.random() > 0.1) return Item.empty
       return item
     })
+
+  // Enigmatic Legacy Plus spellstones: 75% chance to void from any loot table
+  const elSpellstones = [
+    'enigmaticlegacyplus:angel_blessing',
+    'enigmaticlegacyplus:blazing_core',
+    'enigmaticlegacyplus:eye_of_nebula',
+    'enigmaticlegacyplus:forgotten_ice',
+    'enigmaticlegacyplus:golem_heart',
+    'enigmaticlegacyplus:lost_engine',
+    'enigmaticlegacyplus:ocean_stone',
+    'enigmaticlegacyplus:revival_leaf',
+    'enigmaticlegacyplus:void_pearl',
+  ]
+  event.addTableModifier(/.*/)
+    .modifyLoot(elSpellstones, (item) => {
+      if (Math.random() > 0.25) return Item.empty
+      return item
+    })
 })
